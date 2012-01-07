5292f8add1d59bfb01c3f34b15cfc78efea8b9e8
Toru Hisai <toru@torus.jp>
2012-01-07 16:44:13 +0900

    clip

--- GraphicsSample.java
+++ GraphicsSample.java
@@ -42,7 +42,8 @@ class GraphicsDraw extends Frame
             double a2 = angle - Math.PI * 0.5 * sign;
             int x2 = x + (int)(Math.cos(a2) * r2);
             int y2 = y + (int)(Math.sin(a2) * r2);
-            drawTree(buf, n + 1, x2, y2, a2, -sign);
+            if (x2 >= 0 && y2 >= 0 && x2 < 640 && y2 < 480)
+                drawTree(buf, n + 1, x2, y2, a2, -sign);
         }
     }
 }
